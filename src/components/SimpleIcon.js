b2b6b03f9698fe3dda901328a7170013f05cd001
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-02-20 11:19:20 -0300

    Do note use simple-icons

--- src/components/SimpleIcon.js
+++ src/components/SimpleIcon.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Icon from 'react-simple-icons'
-import simpleIcons from 'simple-icons'
 
 const SimpleIcon = (props) => (
   <div className="d-inline-flex">
-    {/* <Icon name={props.icon} fill={props.color || '#212121'} size={props.size || 16} /> */}
-    {console.log(simpleIcons['Medium'])}
+    <Icon name={props.icon} fill={props.color || '#212121'} size={props.size || 16} />
   </div>
 )
 
